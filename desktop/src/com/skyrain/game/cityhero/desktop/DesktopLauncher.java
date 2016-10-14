6fa892fda55e03b5ae6c4510c1cf5df0441b223e
Var_Rain <var_rain@outlook.com>
2016-10-14 23:48:56 +0800

    Var_雨中行

---
+++ desktop/src/com/skyrain/game/cityhero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skyrain.game.cityhero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skyrain.game.cityhero.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
