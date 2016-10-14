2048d67204c4303e4c4f87ac62435dfa84d188ae
Var_Rain <var_rain@outlook.com>
2016-10-15 00:33:22 +0800

    修改了一些配置

--- desktop/src/com/skyrain/game/cityhero/desktop/DesktopLauncher.java
+++ desktop/src/com/skyrain/game/cityhero/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.skyrain.game.cityhero.MainGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "CityHero";
+        config.vSyncEnabled = false;
+        config.width = 960;
+        config.height = 540;
+        config.useGL30 = true;
+        new LwjglApplication(new MainGame(), config);
+    }
 }
